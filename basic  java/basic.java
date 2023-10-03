9edf0a21b1d4f44735818b0ea355fedb9576a1bd
Shailendra Rai <shailendrarai153@gmail.com>
2023-10-03 17:38:20 +0530

    Add files via upload

---
+++ basic  java/basic.java
@@ -0,0 +1,29 @@
+public class basic {
+    public static void main(String[] args) {
+//        System.out.println("  Collage walla");
+//        System.out.println("text in next line");
+//        System.out.println("7,8,9");
+//        System.out.println(7+89+40);
+//        //varible
+//        int x=1;
+//        int y=10;
+//        int z= x+y;
+//        int  a= 10;
+//        System.out.println(a);
+//        a=20; //update
+//        System.out.println("upadte " + a);
+//
+//        System.out.println(z);
+     // Arithemetic operation
+        int x=5,y=9;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+
+
+    }
+
+}
---
+++ basic  java/inceremnt.java
@@ -0,0 +1,27 @@
+public class inceremnt {
+    public static void main(String[] args) {
+//        int x=9;
+//        x++;// x-- ka matlb hai x = x-1
+//        System.out.println(x);
+//        int x =9;
+//        int y =3;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        System.out.println(++y);
+        // double
+//         double x=9;
+//          double y=4;
+//          double z= x/y;
+//        System.out.println(z);
+        //precenatge of five marks
+        double m=100;
+        double e= 92;
+        double h=89;
+        double che= 45;
+        double phy=56;
+        double y= (m+e+h+che+phy)/5;
+        System.out.println( " precentage of number  " +y);
+
+
+    }
+}
